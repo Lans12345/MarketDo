3b02dee8f04d834dfd2b8fb8731f16489c61e39f
Lans12345 <olanalans12345@gmail.com>
2022-10-24 11:13:59 +0800

    Add. search delegate

--- lib/widgets/search_bar_widget.dart
+++ lib/widgets/search_bar_widget.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:marketdo/screens/view_product_page.dart';
 import 'package:marketdo/widgets/text_widget.dart';
 
@@ -50,33 +52,69 @@ class MySearchDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    final box = GetStorage();
     // TODO: implement buildSuggestions
-    return StreamBuilder<Object>(
-        stream: null,
-        builder: (context, snapshot) {
-          return ListView.separated(
-              itemCount: 20,
-              separatorBuilder: ((context, index) {
-                return const Divider();
-              }),
-              itemBuilder: ((context, index) {
-                return ListTile(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ViewProductPage()));
-                  },
-                  leading: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Image.asset('assets/images/googlelogo.png'),
-                  ),
-                  title: TextBold(
-                      text: 'Fresh Saging', fontSize: 18, color: Colors.black),
-                  subtitle: TextRegular(
-                      text: 'Impasugong Bukidnon',
-                      fontSize: 12,
-                      color: Colors.grey),
-                );
-              }));
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('Products')
+            .where('productName', isEqualTo: query)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return const Center(child: Text('Error'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print('waiting');
+            return const Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: Center(
+                  child: CircularProgressIndicator(
+                color: Colors.black,
+              )),
+            );
+          }
+
+          final data = snapshot.requireData;
+          return SizedBox(
+              child: ListView.builder(
+                  itemCount: snapshot.data?.size ?? 0,
+                  itemBuilder: ((context, index) {
+                    return ListTile(
+                      onTap: () {
+                        box.write('address', data.docs[index]['address']);
+                        box.write('category', data.docs[index]['category']);
+                        box.write(
+                            'contactNumber', data.docs[index]['contactNumber']);
+                        box.write('id', data.docs[index]['id']);
+                        box.write('imageURL', data.docs[index]['imageURL']);
+                        box.write('productDescription',
+                            data.docs[index]['productDescription']);
+
+                        box.write(
+                            'productPrice', data.docs[index]['productPrice']);
+                        box.write(
+                            'productName', data.docs[index]['productName']);
+                        box.write('seller', data.docs[index]['seller']);
+                        box.write(
+                            'sellerEmail', data.docs[index]['sellerEmail']);
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ViewProductPage()));
+                      },
+                      leading: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Image.network(data.docs[index]['imageURL']),
+                      ),
+                      title: TextBold(
+                          text: data.docs[index]['productName'],
+                          fontSize: 18,
+                          color: Colors.black),
+                      subtitle: TextRegular(
+                          text: data.docs[index]['address'],
+                          fontSize: 12,
+                          color: Colors.grey),
+                    );
+                  })));
         });
   }
 }
